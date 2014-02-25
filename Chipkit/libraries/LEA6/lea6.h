bcc5d7804e9929e4225bd13e0c9aafa5e696205b
imciner2 <mcianster@gmail.com>
2014-02-24 18:23:05 -0600

    Rewrote LEA6 library to take in reference to serial port.

--- Chipkit/libraries/LEA6/lea6.h
+++ Chipkit/libraries/LEA6/lea6.h
@@ -2,26 +2,13 @@
  * Header file containing class definitions and variables for the LEA6 GPS interface class.
  * 
  * Authored by Ian McInerney, Iowa State University
- * Created for the HABET LX-131-A&B flights in April 2013
  */
 #ifndef LEA6_H_
 #define LEA6_H_
 
 #include <stdint.h>
 #include <stddef.h>
-#include <HardwareSerial.h>
 #include "TinyGPS.h"
-
-// Data structure which can contain all the useful GPS information
-struct UBLOX_RECEIVED_INFO {
-	unsigned long time;			// GPS Time
-	long latitude;				// GPS Latitude
-	long longitude;				// GPS Longitude
-	long altitude;				// GPS altitude above MSL
-	unsigned short satellites;	// # of GPS satellites
-	unsigned long hdop;			// Position dillution of Precision
-	unsigned long fixAge;		// Status of the GPS fix
-};
 
 /**
  * A class to interface with a uBlox LEA6 GPS and retrieve the GPS data.
@@ -32,12 +19,26 @@ struct UBLOX_RECEIVED_INFO {
 class LEA6 {
 
 public:
+
+	// Data structure which can contain all the useful GPS information
+	struct ubloxData_t {
+		unsigned long time;			// GPS Time
+		long latitude;				// GPS Latitude
+		long longitude;				// GPS Longitude
+		long altitude;				// GPS altitude above MSL
+		unsigned short satellites;	// # of GPS satellites
+		unsigned long hdop;			// Position dillution of Precision
+		unsigned long fixAge;		// Status of the GPS fix
+	};
+
+
 	/**
 	 * Standard C++ constructor function.
-	 * This function does not do anything, to configure the GPS you must use the init() function
+	 *
+	 * @param serialPort Reference to the serial port the GPS is on
 	 */
-	LEA6();
-	
+	LEA6(HardwareSerial *serialPort);
+
 	/**
 	 * Function to perform initial configuration for the uBlox GPS.
 	 * It will open the serial port to be 38400BPS, and then configure the UBlox GPS to output only GGA and RMC NMEA strings on its UART1 port
@@ -56,14 +57,15 @@ public:
 	/**
 	 * Function to retrieve the current GPS data
 	 * 
-	 * @return A copy of the UBLOX_RECEIVED_INFO object with the most recent GPS data
+	 * @return A copy of the ubloxData_t object with the most recent GPS data
 	 */
-	UBLOX_RECEIVED_INFO getPositionInfo();
+	ubloxData_t getPositionInfo();
 	
 private:
-	TinyGPS parser;				// TinyGPS library object used to parse the GPS data received
-	UBLOX_RECEIVED_INFO info;	// Position information from the uBlox
-	unsigned long lastTime;		// The timestamp of the last GPS packet received
+	TinyGPS parser;					// TinyGPS library object used to parse the GPS data received
+	ubloxData_t info;				// Position information from the uBlox
+	unsigned long lastTime;			// The timestamp of the last GPS packet received
+	HardwareSerial *serialPort;		// The serial port the GPS is connected on
 };
 
 
